aabab0da10ce610236f291a988b58bc3600b93a5
chengzhuo <cz593682855@163.com>
2016-10-19 15:27:47 +0800

    报价

--- themes/default/static/js/zxbj_new_two.js
+++ themes/default/static/js/zxbj_new_two.js
@@ -96,15 +96,22 @@
     jq('#new_base_info').click(function(){
     	if (validData()) {
         
-      	var mjia = jq('#square').val() * 500;
-        jq('#bprice').html(mjia*0.8);
+      	var mjia = jq('#square').val()*500;
+      	
+      	var mjiaNum=(mjia*0.8/10000).toFixed(1);
+      	
+        jq('#bprice').html(mjiaNum);
+        jq('.bj_res_t span').html("万元");
 
         jq('#materialPay em').html(mjia*0.48);
         jq('#artificialPay em').html(mjia*0.32);
+        
         jq('#designPay em').html(0);
         jq('#designPay').append('<del>'+mjia*0.19+'元'+'</del>')
+        
         jq('#qualityPay em').html(0);
         jq('#qualityPay').append('<del>'+mjia*0.11+'元'+'</del>')
+        
         jq('#new_base_info').css("background-position","0 -168px")
         }
     	else{
